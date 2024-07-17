c0170913815501e6fb4f209a39e2186b4e1597a9
jashwanth-randomwalk <jashwanth.p@randomwalk.ai>
2024-07-17 16:16:53 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+export default function Home() {
+  const handleLogin = () => {
+    // Handle your login logic here
+    // For simplicity, we'll assume the login is successful and then redirect
+    // You might want to implement actual login functionality
+    // and redirect based on the login status
+    // For now, we'll directly redirect to /dashboard after clicking the button
+    window.location.href = "/dashboard";
+  };
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  href="/forgot-password"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Link href="/dashboard">
+
+              <Button className="w-full mb-4">
+                Login
+              </Button>
+            </Link>
+
+            <Link href="/dashboard">
+              <Button variant="outline" className="w-full">
+                Login with Google
+              </Button>
+            </Link>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="#" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/logo.jpg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
